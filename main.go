492b0ba12877627955d80caab4ca716d34edbd2d
Mitchell <davmitchell@au10571.lan>
2018-09-28 09:46:30 +1000

    Naming change

--- main.go
+++ main.go
@@ -4,22 +4,26 @@ import (
 	"os"
 )
 
-//lambdaEnv checks whether code is executing in an AWS Lambda environment
-func lambdaEnv() bool {
+//IsLambdaEnv checks whether code is executing in an AWS Lambda environment
+func IsLambdaEnv() bool {
 	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != "" { //Is there a better approach than this...?
 		return true
 	}
 	return false
 }
 
-func azureFunctionEnv() {}
+// IsAzureFunctionEnv checks whether code is running in an Azure Function environment
+func IsAzureFunctionEnv() {}
 
-func gcpFunctionEnv() {}
+// IsGcpFunctionEnv checks whether code is running in a Google Cloud Platform Function environment
+func IsGcpFunctionEnv() {}
 
-func kubelessEnv() {}
+// IsKubelessEnv checks whether code is running in a Kubless function environment
+func IsKubelessEnv() {}
 
-func serverlessEnv() bool {
-	if lambdaEnv() {
+// IsServerlessEnv checks whether code is running in a serverless environment (any platform)
+func IsServerlessEnv() bool {
+	if IsLambdaEnv() {
 		return true
 	}
 	return false
